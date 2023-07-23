e9e03c1d4cbfc27e8fa4ce3440a60bdf105646f3
jiang1096 <jiang.ml1096@gmail.com>
2023-07-23 15:43:17 +0000

    exp sem 4

--- src/workflow-semillerio/a701_RUN_correr_workflow.r
+++ src/workflow-semillerio/a701_RUN_correr_workflow.r
@@ -8,9 +8,9 @@ options(error = function() {
 
 
 # corrida de cada paso del workflow
-source("~/labo2023r/src/workflow-semillerio/z711_CA_reparar_dataset.r")
-source("~/labo2023r/src/workflow-semillerio/z721_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-semillerio/z731_FE_historia.r")
-source("~/labo2023r/src/workflow-semillerio/z741_TS_training_strategy.r")
-source("~/labo2023r/src/workflow-semillerio/z751_HT_lightgbm.r")
-source("~/labo2023r/src/workflow-semillerio/z771_ZZ_final_semillerio.r")
+source("~/labo2023r/src/workflow-semillerio/a711_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-semillerio/a721_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-semillerio/a731_FE_historia.r")
+source("~/labo2023r/src/workflow-semillerio/a741_TS_training_strategy.r")
+source("~/labo2023r/src/workflow-semillerio/a751_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-semillerio/a771_ZZ_final_semillerio.r")
